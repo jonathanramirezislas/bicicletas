8cdfa113fb421dd5da8605f64eaa66eee14a34c2
Jonathan Ramirez <jonathanalejandro.ramirezislas@gmail.com>
2020-10-24 17:36:59 -0500

    login

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ const tokenRouter = require('./routes/token');
 
 
 
-
-
 const app = express();
 const mongoose = require('mongoose');
 //Conection to the DB
@@ -39,7 +37,6 @@ db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 //we save the session in the server (Note. If the server trun off the session is lost)
 const store = new session.MemoryStore;
-var app = express();
 app.use(session({
   cookie: {maxAge: 240 * 60 * 60 * 1000}, //time for cookie
   store: store,//we save in store
@@ -68,6 +65,84 @@ app.use('/api/bicicletas', bicicletasAPIRouter);
 app.use('/api/usuarios', usuariosAPIRouter);
 app.use('/usuarios', usuariosRouter);
 app.use('/token', tokenRouter);
+
+
+
+//Handle Routes from app.js
+app.get('/login', (req, res)=>{
+  res.render('session/login')
+})
+
+app.post('/login', (req, res, next)=> {
+  //method of passport
+  passport.authenticate('local', (err, user, info)=>{
+    //if there is a error
+    if(err) return next(err);
+    //if there is not a user , we render login and pass info
+    if(!usuario) return res.render('session/login', {info});
+    req.logIn(usuario, err =>{
+      if(err) return next(err);
+      //if everthing is ok redirect to home
+      return res.redirect('/');
+    });
+  }) (req, res, next);
+})
+
+app.get('/logout', (req, res)=>{
+  req.logOut() //clean the session
+  res.redirect('/')
+})
+
+
+app.get('/forgotPassword', (req, res)=>{
+  res.render('session/forgotPassword')
+})
+
+app.post('/forgotPassword', (req, res)=>{
+  Usuario.findOne({email: req.body.email}, (err, usuario)=>{
+    if(!usuario) return res.render('session/forgotPassword', {info: {message: 'No existe la clave'}});
+    
+    usuario.resetPassword(err=>{
+      if(err) return next(err);
+      console.log('session/forgotPasswordMessage');
+    })
+    res.render('session/forgotPasswordMessage')
+  })
+})
+
+app.get('/resetPassword/:token', (req, res, next)=>{
+  Token.findOne({token: req.params.token}, (err, token)=>{
+    if(!token) return res.status(400).send({type: 'not-verified', msg: 'No existe una clave así'})
+
+    Usuario.findById(token._userId, (err, usuario)=>{
+      if(!usuario) return res.status(400).send({msg: 'No existe un usuario asociado a este password'});
+      res.render('session/resetPassword', {errors: {}, usuario: usuario})
+    })
+  })
+})
+
+app.post('/resetPassword', (req, res)=>{
+  if(req.body.password != req.body.confirm_password) {
+    res.render('session/resetPassword', {errors: {confirm_password: {message: 'No coinciden las contraseñas'}}});
+    return;
+  }
+  Usuario.findOne({email: req.body.email}, (err, usuario)=>{
+    usuario.password = req.body.password;
+    usuario.save(err=>{
+      if(err){
+        res.render('session/resetPassword', {errors: err.errors, usuario: new Usuario});
+      } else {
+        res.redirect('/login')
+      }
+    })
+  })
+})
+//end of handle routes from app.js
+
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- config/passport.js
+++ config/passport.js
@@ -2,20 +2,27 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const Usuario = require('../models/usuario');
 
+
 passport.use(new LocalStrategy(
     (email, password, done)=>{
+        //we search the user by his/her email
         Usuario.findOne({email: email}, (err, usuario)=>{
+            //If there is a error
             if(err) return done(err)
+            //If there is not user with taht email
             if(!usuario) return done(null, false, {message: 'Email no existente o incorrecto'});
+            //Is password is not valid
             if (!usuario.validPassword(password)) return done(null, false, {message: 'Password no existente o incorrecto'});
 
+            //Everthing is OK. Execute the callaback
             return done(null, usuario)
         })
     }
 ));
 
-passport.serializeUser(user, cb => {
-    cb(null, user.id);
+//cb(callback)
+passport.serializeUser(cb => {
+    cb(null, usuario.id);
 });
 
 passport.deserializeUser((id, cb)=>{
