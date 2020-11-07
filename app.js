9b6c1ea0073c29009e71684a40d86271e1d4d366
Jonathan Ramirez <jonathanalejandro.ramirezislas@gmail.com>
2020-11-07 14:22:12 -0600

    api scoep google

--- app.js
+++ app.js
@@ -183,8 +183,8 @@ app.get(
   "/auth/google",
   passport.authenticate("google", {
     scope: [
-      "https://www.googleapis.com/auth/userinfo.profile",
-      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/plus.login",
+      "https://www.googleapis.com/auth/plus.profile.emails.read",
     ],
     accessType: "offline",
     approvalPrompt: "force",
